d5702f89f164af31e8a29b8f9655b2ab1307a11d
Dima Tisnek <dimaqq@gmail.com>
2024-07-04 13:25:12 +0900

    wip: add ingress lib

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-<!--
-Avoid using this README file for information that is maintained or published elsewhere, e.g.:
-
-* metadata.yaml > published on Charmhub
-* documentation > published on (or linked to from) Charmhub
-* detailed contribution guide > documentation or CONTRIBUTING.md
-
-Use links instead.
--->
-
 # hexanator
 
 Charmhub package name: `haxanator`
 More information: https://charmhub.io/hexanator
 
 Hexanator charm wraps Gubernator, a high-performance, distributed rate-limiting service.
+
+## To Do
+
+- [ ] document, at high level, how gubernator works
+- [ ] document how it's mapped to juju primitives
+- [ ] document workers, resource limits, scaling
+- [ ] document how to connect to gubernator, or how to put it behind a load balancer
--- src/charm.py
+++ src/charm.py
@@ -7,6 +7,8 @@ import logging
 
 import ops
 
+from charms.traefik_k8s.v2.ingress import IngressPerAppRequirer
+
 # Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
 
@@ -18,9 +20,10 @@ class HexanatorCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+        # TODO: presumably the lib figures out app name and model name
+        # TODO: check if this is the correct port
+        self.ingress = IngressPerAppRequirer(self, port=80)
         self.framework.observe(self.on['gubernator'].pebble_ready, self._on_gubernator_pebble_ready)
-        # FIXME I feel these are not needed
-        # self.framework.observe(self.on.config_changed, self._on_config_changed)
 
     def _on_gubernator_pebble_ready(self, event: ops.PebbleReadyEvent):
         """Kick off Pebble services.
@@ -32,54 +35,6 @@ class HexanatorCharm(ops.CharmBase):
         # Get a reference the container attribute on the PebbleReadyEvent
         event.workload.replan()
         self.unit.status = ops.ActiveStatus()
-
-    def _unused_on_config_changed(self, event: ops.ConfigChangedEvent):
-        """Handle changed configuration.
-
-        Change this example to suit your needs. If you don't need to handle config, you can remove
-        this method.
-
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        # Fetch the new config value
-        log_level = self.model.config["log-level"].lower()
-
-        # Do some validation of the configuration option
-        if log_level in VALID_LOG_LEVELS:
-            # The config is good, so update the configuration of the workload
-            container = self.unit.get_container("gubernator")
-            # Verify that we can connect to the Pebble API in the workload container
-            if container.can_connect():
-                # Push an updated layer with the new config
-                container.add_layer("gubernator", self._pebble_layer, combine=True)
-                container.replan()
-
-                logger.debug("Log level changed to %r", log_level)
-                self.unit.status = ops.ActiveStatus()
-            else:
-                # We were unable to connect to the Pebble API, so we defer this event
-                event.defer()
-                self.unit.status = ops.WaitingStatus("waiting for Pebble API")
-        else:
-            # In this case, the config option is bad, so block the charm and notify the operator.
-            self.unit.status = ops.BlockedStatus("invalid log level: {log_level!r}")
-
-    @property
-    def _pebble_layer(self) -> ops.pebble.LayerDict:
-        """Return a dictionary representing a Pebble layer."""
-        return {
-            "summary": "gubernator layer",
-            "description": "pebble config layer for gubernator",
-            "services": {
-                "gubernator": {
-                    "override": "replace",
-                    "summary": "gubernator",
-                    "command": "/bin/gubernator",
-                    "startup": "enabled",
-                    "environment": {"LOG_LEVEL": "info"},
-                }
-            },
-        }
 
 
 if __name__ == "__main__":  # pragma: nocover
