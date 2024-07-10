4045c629337b5b281335192f5b36e08c179091c4
Dima Tisnek <dimaqq@gmail.com>
2024-07-10 15:22:14 +0900

    test: document gubernator 3.0.0 request format

---
+++ tests/test_smoke.py
@@ -0,0 +1,15 @@
+
+"""
+root@hexanator-0:/# curl http://localhost:80/v1/rate-limit.check --header 'Content-Type: application/json'   --data '{
+    "requests": [
+        {
+            "name": "requests_per_sec",
+            "unique_key": "account:12345",
+            "hits": "1",
+            "limit": "10",
+            "duration": "1000"
+        }
+    ]
+}'
+{"responses":[{"limit":"10", "remaining":"9", "reset_time":"1720592294506"}]}
+"""
