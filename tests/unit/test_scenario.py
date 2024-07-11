9358ef82cad95e53c557a0ebc53e25ba0a451266
Dima Tisnek <dimaqq@gmail.com>
2024-07-11 17:24:57 +0900

    fix(tests): keep pyright happy; validate replan()

--- tests/unit/test_scenario.py
+++ tests/unit/test_scenario.py
@@ -9,28 +9,20 @@ def default_pebble_layer() -> dict:
     import yaml
     tmp = yaml.safe_load(open("rockcraft.yaml").read())
     assert tmp["services"]["gubernator"]["startup"] == "enabled"
+    return tmp
 
 
 def test_startup():
     ctx = Context(HexanatorCharm)
-    pebble_layers = {"default": ops.pebble.Layer(raw=default_pebble_layer())}
+    pebble_layers = {"default": ops.pebble.Layer(raw=default_pebble_layer())}  # type: ignore
     container = Container(name="gubernator", can_connect=True, layers=pebble_layers)
     relation=Relation(endpoint="ingress", interface="ingress", remote_app_name="ingress")
-    state = State( leader=True, relations=[relation], containers=[container])
+    state = State( leader=True, relations={relation}, containers={container})  # type: ignore
 
     state = ctx.run(ctx.on.start(), state)
-    state = ctx.run(ready_event := ctx.on.pebble_ready(container), state)
+    state = ctx.run(ctx.on.pebble_ready(container), state)
     state = ctx.run(ctx.on.relation_joined(relation), state)
 
     assert state.unit_status == ops.ActiveStatus()
     assert relation.local_app_data == {"model": ANY, "name": '"hexanator"', "port": "80", "strip-prefix": "true"}
-
-    # FIXME: can't find anything useful to assert on...
-    # - (ops.charm.PebbleReadyEvent).workload._pebble._service_status got updated on replan()
-    # - but that bit of info seem lost, scenario.state._Event doesn't get updated
-    # - state.containers[0].service_status doesn't get updated
-    # https://github.com/canonical/ops-scenario/issues/152
-
-    # assert ready_event.workload._pebble._service_status["gubernator"] == "active"
-
-    assert ready_event  # keep ruff happy
+    assert list(state.containers)[0].service_status["gubernator"] == ops.pebble.ServiceStatus.ACTIVE
