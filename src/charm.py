c782cb398d61573a4c8eef131487c92f4f1aa74a
Dima Tisnek <dimaqq@gmail.com>
2025-06-27 13:27:42 +0900

    wip

--- src/charm.py
+++ src/charm.py
@@ -5,6 +5,7 @@
 
 import json
 import logging
+import pathlib
 import socket
 import typing
 
@@ -25,7 +26,9 @@ class TracingIntegrationTester(ops.CharmBase):
         self.framework.observe(self.on.tracing_relation_departed, self.reconcile)
         self.framework.observe(self.on.tracing_relation_broken, self.reconcile)
 
-        self.framework.observe(self.on.get_traces_action, self.get_traces_action)
+        self.framework.observe(self.on.list_traces_action, self.list_traces)
+        self.framework.observe(self.on.drop_traces_action, self.drop_traces)
+        self.framework.observe(self.on.read_trace_action, self.read_trace)
 
     def reconcile(self, event: typing.Any):
         if not self.unit.is_leader():
@@ -55,11 +58,44 @@ class TracingIntegrationTester(ops.CharmBase):
 
         self.model.app.status = ops.ActiveStatus(str(servers))
 
-    def get_traces_action(self, event: ops.ActionEvent):
-        # FIXME implement filter by start, end, application
-        # event.params ...
-        traces = [str(p) for p in server.datadir.glob("*.json")]
-        event.set_results({"traces": json.dumps(traces)})
+    def list_traces(self, event: ops.ActionEvent):
+        apps = [a.strip() for a in event.params.get("apps", "").split(",") if a.strip()]
+        start = event.params.get("start", float("-inf"))
+        end = event.params.get("end", float("inf"))
+        paths = [t for t in server.datadir.glob("*.json") if fixme(t, apps, start, end)]
+        event.set_results({"traces": json.dumps(map(str, paths))})
+
+    def drop_traces(self, event: ops.ActionEvent):
+        apps = [a.strip() for a in event.params.get("apps", "").split(",") if a.strip()]
+        start = event.params.get("start", float("-inf"))
+        end = event.params.get("end", float("inf"))
+        paths = [t for t in server.datadir.glob("*.json") if fixme(t, apps, start, end)]
+        for path in paths:
+            try:
+                path.unlink()
+            except FileNotFoundError:
+                pass
+        event.set_results({})
+
+    def read_trace(self, event: ops.ActionEvent):
+        path = pathlib.Path(event.params["path"])
+        try:
+            assert path.is_relative_to(server.datadir)
+            event.set_results({"content": path.read_text()})
+        except Exception as e:
+            event.fail(repr(e))
+
+
+def fixme(path: pathlib.Path, apps: list[str], start: float, end: float):
+    """Determine if a particular trace file matches the filters."""
+    try:
+        app, ts = path.stem.split("-")
+        time = float(ts)
+    except Exception:
+        logging.warning("Skipping data file %r", path)
+        return False
+
+    return start < time < end and (app in apps or not apps)
 
 
 if __name__ == "__main__":
